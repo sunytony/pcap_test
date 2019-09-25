511eac88136d5f05450956108974c2f5619d455f
yoonseungmin <sunytony@naver.com>
2019-09-25 10:59:45 +0900

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -27,38 +27,38 @@ void packet_analysis(u_char* args, const struct pcap_pkthdr* header, const u_cha
 	print_len(packet+6,6);
   	uint16_t type_IP = my_ntohs((uint16_t)(packet[12]));
 	if(type_IP == 0x800){
-	    printf("\nsource ip : ");
-	    print_len(packet+ether_len+12,4);
-	    printf("\ndest ip : ");
-	    print_len(packet+ether_len+16,4);
-	    ip_len = (packet[ether_len]&15)*4;
-	    printf("\nIP header length = %d ",ip_len);
-	    type_TCP = packet[ether_len+9];
-	    test = (uint16_t*)(packet+2+ether_len);
-	    total_len = my_ntohs(*test);
-	    if(type_TCP == 6){
-	   	uint16_t src_port, dest_port;
-	   	test = (uint16_t*)(packet+ether_len+ip_len);
-	     	src_port = my_ntohs(*test);
-	      	test = (uint16_t*)(packet+ether_len+ip_len+2);
-	     	dest_port = my_ntohs(*test);
-	      	printf("\nsource port : %d\ndest port : %d", src_port, dest_port);
-	     	tcp_len = (packet[ether_len+ip_len+12]&0xF0)/16*4;
-	     	printf("\ntcp header length = %d\n",tcp_len);
-	      	data_len = total_len-ip_len-tcp_len;
-	      	if(data_len>0){
-	      		printf("data : ");
-	      		if(data_len<32){
-	      			for(int i=0; i<data_len; i++)
-	   					printf("%x ", packet[ether_len+ip_len+tcp_len+i]);
-	      		}
-	      		else{
-	      			for(int i=0; i<32; i++)
-	    				printf("%x ", packet[ether_len+ip_len+tcp_len+i]);
-	      		}
-	      	}
+		printf("\nsource ip : ");
+		print_len(packet+ether_len+12,4);
+		printf("\ndest ip : ");
+		print_len(packet+ether_len+16,4);
+		ip_len = (packet[ether_len]&15)*4;
+		printf("\nIP header length = %d ",ip_len);
+		type_TCP = packet[ether_len+9];
+		test = (uint16_t*)(packet+2+ether_len);
+		total_len = my_ntohs(*test);
+	    	if(type_TCP == 6){
+		   	uint16_t src_port, dest_port;
+		   	test = (uint16_t*)(packet+ether_len+ip_len);
+		     	src_port = my_ntohs(*test);
+		      	test = (uint16_t*)(packet+ether_len+ip_len+2);
+		     	dest_port = my_ntohs(*test);
+		      	printf("\nsource port : %d\ndest port : %d", src_port, dest_port);
+		     	tcp_len = (packet[ether_len+ip_len+12]&0xF0)/16*4;
+		     	printf("\ntcp header length = %d\n",tcp_len);
+		      	data_len = total_len-ip_len-tcp_len;
+		      	if(data_len>0){
+		      		printf("data : ");
+		      		if(data_len<32){
+		      			for(int i=0; i<data_len; i++)
+		   					printf("%x ", packet[ether_len+ip_len+tcp_len+i]);
+		      		}
+		      		else{
+		      			for(int i=0; i<32; i++)
+		    				printf("%x ", packet[ether_len+ip_len+tcp_len+i]);
+		      		}
+		      	}
 
-	     } 
+	     	} 
 	}
 	printf("\n");
 }
